e158bb8a2e9ab00540aaf42d2740f8b8fbaefc45
Andy Thompson <athompson@inviqa.com>
2015-05-12 20:00:13 +0100

    Add a etc-local recipe to generate Magento local.xml

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,45 @@
+default['magento']['db']['host'] = "localhost"
+default['magento']['db']['database'] = "magentodb"
+default['magento']['db']['username'] = "magentouser"
+default['magento']['db']['model'] = "mysql4"
+default['magento']['db']['read']['host'] = "localhost"
+default['magento']['db']['write']['host'] = "localhost"
+
+default['magento']['app']['base_path'] = "public"
+default['magento']['app']['locale'] = "en_GB"
+default['magento']['app']['timezone'] = "Europe/London"
+default['magento']['app']['currency'] = "GBP"
+default['magento']['app']['session_save'] = "db" # files|db|memcache
+default['magento']['app']['admin_frontname'] = "admin"
+default['magento']['app']['use_rewrites'] = "yes"
+default['magento']['app']['use_secure'] = "yes"
+default['magento']['app']['use_secure_admin'] = "yes"
+default['magento']['app']['multi_session_save'] = "db" # files|db|memcache|redis
+default['magento']['app']['session_memcache_ip'] = "127.0.0.1"
+default['magento']['app']['session_memcache_port'] = "11211"
+default['magento']['app']['backend_cache'] = "file" # apc|memcached|xcache|file|Cm_Cache_Backend_Redis
+default['magento']['app']['slow_backend'] = "database" # database|file
+default['magento']['app']['backend_servers'] = Array.new
+
+default['magento']['redis']['host'] = '127.0.0.1'
+default['magento']['redis']['port'] = '6379'
+default['magento']['redis']['timeout'] = '2.5'
+default['magento']['redis']['database'] = '0'
+default['magento']['redis']['full_page_cache_database'] = '1'
+default['magento']['redis']['session_database'] = '2'
+default['magento']['redis']['force_standalone'] = '0'
+default['magento']['redis']['automatic_cleaning_factor'] = '0'
+default['magento']['redis']['compress_data'] = '1'
+default['magento']['redis']['compress_tags'] = '1'
+default['magento']['redis']['compress_threshold'] = '2048'
+default['magento']['redis']['compression_lib'] = 'gzip'
+
+default['magento']['global']['custom'] = ''
+default['magento']['global']['extra_params'] = {
+  'skip_process_modules_updates' => 1,
+  'skip_process_modules_updates_dev_mode' => 1,
+}
+
 %w( apache nginx ).each do |type|
   default[type]['shared_config']['magento'] = {
     'clustered' => false,
@@ -20,6 +62,11 @@
       'owner' => 'deploy',
       'group' => 'deploy',
       'shared_folders' => {
+        'standard' => {
+          'folders' => [
+            'public/app/etc/'
+          ]
+        },
         'writeable' => {
           'owner' => 'apache',
           'group' => 'apache',
---
+++ recipes/etc-local.rb
@@ -0,0 +1,41 @@
+%w( apache nginx ).each do |type|
+  node[type]['sites'].each_pair do |name, site|
+    next unless site['type'] == 'magento'
+
+    magento = ConfigDrivenHelper::Util::immutablemash_to_hash(node['magento'])
+
+    if site['magento']
+      magento = ::Chef::Mixin::DeepMerge.hash_only_merge(
+        magento,
+        ConfigDrivenHelper::Util::immutablemash_to_hash(site['magento']))
+    end
+
+    if Chef::Config[:solo]
+      missing_attrs = %w[
+        crypt_key
+      ].select { |attr| magento[attr].nil? }.map { |attr| %Q{node['#{type}']['sites']['#{name}']['magento']['#{attr}']} }
+
+      unless missing_attrs.empty?
+        Chef::Application.fatal! "You must set #{missing_attrs.join(', ')} in chef-solo mode."
+      end
+    else
+      # generate all passwords
+      node.set_unless['#{type}']['sites']['#{name}']['magento']['#{attr}'] = secure_password
+      node.save
+    end
+
+    if site['capistrano']
+      config_path = "#{site['capistrano']['deploy_to']}/shared/#{magento['base_path']}"
+    else
+      config_path = site['docroot']
+    end
+
+    template "#{config_path}/app/etc/local.xml" do
+      source "magento-local.xml.erb"
+      mode 0644
+      variables({
+        :magento => magento
+      })
+    end
+  end
+end

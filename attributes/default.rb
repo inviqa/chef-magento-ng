43e64fa41c28a3923069df2303147113ebb1015d
Andy Thompson <athompson@inviqa.com>
2015-05-12 19:54:11 +0100

    Add a magento site shared_config.
    * includes appropriate restricted and static directories
    * creates the appropriate writeable magento folders

---
+++ attributes/default.rb
@@ -0,0 +1,35 @@
+%w( apache nginx ).each do |type|
+  default[type]['shared_config']['magento'] = {
+    'type' => 'magento',
+    'protocols' => ['http', 'https'],
+    'restricted_dirs' => [
+      '/app/',
+      '/includes/',
+      '/lib/',
+      '/media/downloadable/',
+      '/pkginfo/',
+      '/report/config.xml',
+      '/var/'
+    ],
+    'static_dirs' => [
+      '/media/',
+      '/skin/'
+    ],
+    'capistrano' => {
+      'owner' => 'deploy',
+      'group' => 'deploy',
+      'shared_folders' => {
+        'writeable' => {
+          'owner' => 'apache',
+          'group' => 'apache',
+          'folders' => [
+            'public/./media',
+            'public/./sitemaps',
+            'public/./staging',
+            'public/./var'
+          ]
+        }
+      }
+    }
+  }
+end
--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,5 @@ license          'All rights reserved'
 description      'Installs/Configures magento-ng'
 long_description 'Installs/Configures magento-ng'
 version          '0.1.0'
+
+depends 'config-driven-helper', '~> 1.3.5'

f8f5bac2a6a498c235451e2c3e4dd541de049e75
Andy Thompson <athompson@inviqa.com>
2015-07-01 20:57:14 +0100

    Fix location that Magento app attributes are accessed

--- recipes/etc-local.rb
+++ recipes/etc-local.rb
@@ -13,19 +13,19 @@
     if Chef::Config[:solo]
       missing_attrs = %w[
         crypt_key
-      ].select { |attr| magento[attr].nil? }.map { |attr| %Q{node['#{type}']['sites']['#{name}']['magento']['#{attr}']} }
+      ].select { |attr| magento['app'][attr].nil? }.map { |attr| %Q{node['#{type}']['sites']['#{name}']['magento']['app']['#{attr}']} }
 
       unless missing_attrs.empty?
         raise "You must set #{missing_attrs.join(', ')} in chef-solo mode."
       end
     else
       # generate all passwords
-      node.set_unless['#{type}']['sites']['#{name}']['magento']['#{attr}'] = secure_password
+      node.set_unless['#{type}']['sites']['#{name}']['magento']['app']['#{attr}'] = secure_password
       node.save
     end
 
     if site['capistrano']
-      config_path = "#{site['capistrano']['deploy_to']}/shared/#{magento['base_path']}"
+      config_path = "#{site['capistrano']['deploy_to']}/shared/#{magento['app']['base_path']}"
     else
       config_path = site['docroot']
     end

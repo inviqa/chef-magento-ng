75f7572321af3b94574051b3020a88e4b9bd265c
Andy Thompson <athompson@inviqa.com>
2015-09-21 17:10:17 +0100

    Bump up minor version for release

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'athompson@inviqa.com'
 license          'All rights reserved'
 description      'Installs/Configures magento-ng'
 long_description 'Installs/Configures magento-ng'
-version          '0.2.1'
+version          '0.3.0'
 
 depends 'config-driven-helper', '~> 1.4'
 depends 'cron', '~> 1.4'

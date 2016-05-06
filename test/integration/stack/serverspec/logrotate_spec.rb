2608b2953eacf6d7221a6ff76b138f9238788aa9
Andy Thompson <athompson@inviqa.com>
2016-05-06 15:31:16 +0100

    Update config-driven-helper to allow for version >= 1.5, < 3.0

--- test/integration/stack/serverspec/logrotate_spec.rb
+++ test/integration/stack/serverspec/logrotate_spec.rb
@@ -5,7 +5,7 @@ set :path, '/sbin:/usr/local/sbin:$PATH'
 describe 'magento-ng::logrotate' do
   describe file('/etc/logrotate.d/magento-site1') do
     it { should be_file }
-    its(:content) { should match %r{^"/docroot/var/log/\*\.log" \{$} }
+    its(:content) { should match %r{^"/var/www/app/shared/public/var/log/\*\.log" \{$} }
     its(:content) { should match(/^\s*weekly$/) }
     its(:content) { should match(/^\s*rotate 4$/) }
   end

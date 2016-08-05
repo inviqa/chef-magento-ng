dda1eeec923b4621661a824a2dfdd33e464e1408
andytson-inviqa <andytson-inviqa@users.noreply.github.com>
2016-08-05 16:49:01 +0100

    Remove ignored logrotate missingok option
    This was silently ignored as only was ever accepted as an `['options'] = ['missingok']`.
    
    logrotate v2.0 changed the definition to a resource, which made all of its arguments stricter.
    
    Since the default options include missingok, this option needn't be specified

--- attributes/default.rb
+++ attributes/default.rb
@@ -53,7 +53,6 @@ default['magento']['aoe_scheduler']['default']['minute'] = '*'
 default['magento']['aoe_scheduler']['watchdog']['mode'] = 'watchdog'
 default['magento']['aoe_scheduler']['watchdog']['minute'] = '*/10'
 
-default['magento']['logrotate']['missingok'] = true
 default['magento']['logrotate']['frequency'] = 'weekly'
 default['magento']['logrotate']['rotate'] = 4
 

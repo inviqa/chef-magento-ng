7a881a71e43226448830cbc81a5a64a113f40800
Andy Thompson <athompson@inviqa.com>
2016-09-14 11:00:26 +0100

    Update ruby scripts to be happy with latest Rubocop style checks

--- recipes/logrotate.rb
+++ recipes/logrotate.rb
@@ -17,7 +17,7 @@
 # limitations under the License.
 #
 
-%w( apache nginx ).each do |type|
+%w(apache nginx).each do |type|
   node[type]['sites'].each_pair do |name, site|
     next unless site['type'] == 'magento'
 
@@ -26,7 +26,8 @@
     if site['magento']
       magento = ::Chef::Mixin::DeepMerge.hash_only_merge(
         magento,
-        ConfigDrivenHelper::Util.immutablemash_to_hash(site['magento']))
+        ConfigDrivenHelper::Util.immutablemash_to_hash(site['magento'])
+      )
     end
 
     config_path = if site['capistrano']
